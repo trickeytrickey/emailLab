c59f14f101abecf525b5f09b6c8580370a194614
trickeytrickey <trickeyc@umich.edu>
2016-02-09 13:24:01 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+//This file is validating as HTML5
+//You need to use and HTML 5 validator to check your code
+
+echo("<?xml version=\"1.0\" encoding=\"UTF-8\"?");
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta charset="utf-8" />
+    <title>Lab 5 - Index</title>
+</head>
+
+<body>
+<h1 style="text-align:center">Lab 5 - Index</h1>
+<?php 
+	include("includes/menu.php");
+?>
+<div style="font-style:italic; text-align:center; font-size:9px;">This set of pages validates as HTML5 compliant<br/>&nbsp;</div>
+<div style="text-align:center">Welcome to my Lab05 site!</div>
+</body>
+</html>
